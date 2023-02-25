50b7a49badaf48405d25e2a0f35e7716fc43fe3f
YhyaGithub <yhyamobile57@gmail.com>
2023-02-25 09:42:41 +0200

    feat(pages): create the main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,5 @@
+<script lang="ts">
+  import '../styles/globals.scss';
+</script>
+
 <slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import '../styles/home.scss';
+</script>
+
+<main>
+	<h1>Notte</h1>
+	<p>write, read and edit notes on any device using notte.</p>
+	<button>Get Started</button>
+</main>
